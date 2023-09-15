dd2a5a39dc3f04fec5063b817aa2154b08ac80ea
estefan <estefanhu074@gmail.com>
2023-09-15 11:21:07 -0700

    tree traversal recusive

--- binaryTree.py
+++ binaryTree.py
@@ -19,3 +19,28 @@ class Solution:
                     return True
             return False
         return (dfs(*start))
+
+    def levelOrderRecursive(self, root):
+        levels = []
+        if not root:
+            return levels
+
+        def helper(node, level):
+            if len(levels) == level:
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left:
+                helper(node.left, level + 1)
+            if node.right:
+                helper(node.right, level + 1)
+
+        helper(root, 0)
+        return levels
+
+    def levelOrderIterative(self, root)
+
+
+
+
